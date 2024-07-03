cce6450fa3c8b211933f66d9fd4fd03aa7610108
AMRI BADR <amribadr17@gmail.com>
2024-07-03 09:13:35 +0000

    bismilah

--- gestion/settings.py
+++ gestion/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     }
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
