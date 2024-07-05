85954e81885f9864d0dd7d4f0cc36fec56d4b3cc
nizar <nizarakka@gmai.com>
2024-07-05 10:08:57 +0100

    push 2

--- Gcom/GcomApp/urls.py
+++ Gcom/GcomApp/urls.py
@@ -1,6 +1,10 @@
+# urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  # Assuming you have an index view
+    path('client/', views.client_view, name='client'),  # New URL pattern for client view
+    # Add other URL patterns here
 ]
--- Gcom/GcomApp/views.py
+++ Gcom/GcomApp/views.py
@@ -1,4 +1,8 @@
+# views.py
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def client_view(request):
+    return render(request, 'client.html')
