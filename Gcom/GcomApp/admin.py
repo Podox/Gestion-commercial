cb04b67fb916447b58e4c6c2844153df3b5c75ca
AMRI BADR <amribadr17@gmail.com>
2024-07-05 10:03:12 +0000

    table made with admin acess

--- Gcom/GcomApp/admin.py
+++ Gcom/GcomApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Client, Command, Offre
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Command)
+admin.site.register(Offre)
