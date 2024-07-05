3c31f890cfaabffc565e7bb5a0e94c4371b94b2b
AMRI BADR <amribadr17@gmail.com>
2024-07-05 09:23:07 +0000

    models

--- Gcom/GcomApp/models.py
+++ Gcom/GcomApp/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Client(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    entreprise = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=200)
+    mail = models.EmailField()
+    numero_telephone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
+
+class Command(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    commande = models.TextField()
+
+    def __str__(self):
+        return f"Commande {self.id} for {self.client}"
