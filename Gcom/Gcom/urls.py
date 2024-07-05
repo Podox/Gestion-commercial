8afc5ff9bc3dc166dce2f7059224d3d0a70b4174
nizar <nizarakka@gmai.com>
2024-07-05 12:38:08 +0100

    last one

--- Gcom/Gcom/urls.py
+++ Gcom/Gcom/urls.py
@@ -8,6 +8,7 @@ from GcomApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login_view, name='login'),
+    path('index/', login_required(views.index), name='index'),  # Protect the index view
     # Include the app's URLs
     path('app/', include('GcomApp.urls')),
 ]
